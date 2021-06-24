f0774e7fb00030c742dd407d37a83ac81fba368a
LifeInvader <matthewkausfox@hotmail.com>
2021-06-24 16:23:44 -0600

    added functionality to app.js program runs, need to know if constructors work

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
-const Start = require('./lib/Start');
+const inquirer = require('inquirer');
 
-new Start().initializeStart();
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+inquirer.prompt({
+    name: "mainMenu",
+    type: "list",
+    message: "What employee would you like to add?",
+    choices: ["Add an Intern", "Add a Manager", "Add an Engineer"]
+}).then((answer) => {
+    switch (answer.mainMenu) {
+        case "Add an Intern":
+            addIntern()
+            break;
+    }
+    switch (answer.mainMenu) {
+        case "Add a Manager":
+            addManager()
+            break;
+    }
+    switch (answer.mainMenu) {
+        case "Add an Engineer":
+            addEngineer()
+            break;
+    };
+});
+
+
+function addIntern() {
+    console.log("nice")
+}
+function addManager() {
+    console.log("very nice")
+}
+function addEngineer() {
+    console.log("Very very nice")
+}
